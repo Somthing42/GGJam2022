208d941877b92c9087abcbac392b523faed57289
Josh Cazel <somthing42@gmail.com>
2022-01-28 21:46:49 -0700

    good stuff
    added text boxes, fonts, fixed some stuff for the quest line and built the ending framework

---
+++ GameJamPrep/Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int questsCompleated = 0;
+
+    public float endTimer;
+
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            //game manager is not destroyed when changing scenes
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            //destroys copies so that only one is in a scene at a time
+            Destroy(gameObject);
+        }
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (player)
+        {
+            if (questsCompleated == 4 & player.GetComponent<PlayerController>().enabled == true)
+            {
+                endTimer -= Time.deltaTime;
+
+                if (endTimer <= 0)
+                {
+                    GameEnd();
+                }
+
+            }
+        }
+    }
+
+    void GameEnd()
+    {
+
+        //Debug.Log("its over!");
+        SceneManager.LoadScene("Ending");
+        
+    }
+}
